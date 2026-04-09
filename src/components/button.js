b60fb2f8843c91b7430b055a3323b791219f7b8f
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-09 21:05:46 +0200

    Add onClick event to button

--- src/components/button.js
+++ src/components/button.js
@@ -1,5 +1,5 @@
-export default function Button({children}) {
+export default function Button({children, onClick}) {
     return (
-        <button>{children}</button>
+        <button className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700" onClick={onClick}>{children}</button>
     )
 }
