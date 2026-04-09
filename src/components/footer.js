c89793f4e91c2d717325db26585fed314d40e0e2
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-09 18:29:36 +0200

    Add footer component to use on all pages of the app

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+/**
+ * Component to display the footer on all pages of the app.
+ * @returns The footer component.
+ */
+export default function Footer() {
+    return (
+        <footer className="w-full h-16 bg-gray-800 text-white flex items-center justify-center">
+            <p className="text-sm font-bold">WT Assignment</p>
+        </footer>
+    )
+}
