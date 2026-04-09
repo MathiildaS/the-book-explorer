daa7ddaef870a9965cb8e4dc6f29e29ec4d0cef7
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-09 21:07:36 +0200

    Add use client to render component on client side. Add loginBtn function and call when click on login button

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,20 @@
+"use client"
+
+import Button from "./button.js"
+
+
 /**
  * Component to display the header on all pages of the app.
  * @returns The header component.
  */
 export default function Header() {
+    function loginBtn() {
+        //logga in på github
+    }
     return (
         <header className="w-full h-16 bg-gray-800 text-white flex items-center justify-center">
             <h1 className="text-xl font-bold">WT Assignment Dashboard</h1>
+            <nav><Button onClick={loginBtn}>Login with GitHub</Button></nav>
         </header>
     )
 }
