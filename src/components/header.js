528cc5e3f388375deb4ac3ce6ab51140d9a0c3e3
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-09 18:29:17 +0200

    Add header component to use on all pages of the app

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+/**
+ * Component to display the header on all pages of the app.
+ * @returns The header component.
+ */
+export default function Header() {
+    return (
+        <header className="w-full h-16 bg-gray-800 text-white flex items-center justify-center">
+            <h1 className="text-xl font-bold">WT Assignment Dashboard</h1>
+        </header>
+    )
+}
