f32447678d6ea287f39ad593eb221184c75389ce
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-10 20:57:24 +0200

    Refactor test and add GitHub OAuth redirect route

--- src/app/api/githubAuth/route.js
+++ src/app/api/githubAuth/route.js
@@ -1,3 +1,6 @@
 export async function GET() {
-  return Response.json({ message: 'Hello World' })
+  const clientID = process.env.CLIENT_ID;
+  const redirectURI = process.env.REDIRECT_URL;
+  const githubAuthURL = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&scope=read:user`;
+  return Response.redirect(githubAuthURL);
 }
