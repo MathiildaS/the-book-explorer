9782c198bb27131e365ed9a21a771d3b08c613f9
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-10 17:35:12 +0200

    Add redirect to api/githubAuth

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,21 @@
-"use client"
+"use client";
 
-import Button from "./button.js"
-
+import Button from "./button.js";
 
 /**
  * Component to display the header on all pages of the app.
  * @returns The header component.
  */
 export default function Header() {
-    function loginBtn() {
-        //logga in på github
-    }
-    return (
-        <header className="w-full h-16 bg-gray-800 text-white flex items-center justify-center">
-            <h1 className="text-xl font-bold">WT Assignment Dashboard</h1>
-            <nav><Button onClick={loginBtn}>Login with GitHub</Button></nav>
-        </header>
-    )
+  function loginBtn() {
+    window.location.href = "/api/githubAuth";
+  }
+  return (
+    <header className="w-full h-16 bg-gray-800 text-white flex items-center justify-center">
+      <h1 className="text-xl font-bold">WT Assignment Dashboard</h1>
+      <nav>
+        <Button onClick={loginBtn}>Login with GitHub</Button>
+      </nav>
+    </header>
+  );
 }
