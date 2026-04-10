ce2164f5e847b7aabdd4a65afccf4dad81ae520e
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-10 22:35:06 +0200

    Improve error handling. Fetch user data with provided access token

--- src/app/api/githubAuth/callback/route.js
+++ src/app/api/githubAuth/callback/route.js
@@ -5,41 +5,74 @@
  * @returns {Response} the response object with the access token or error message.
  */
 export async function GET(req) {
-  const githubCode = req.nextUrl.searchParams.get("code");
-  
-  if (!githubCode) {
-    return new Response("No session code provided", { status: 400 });
-  }
+  try {
+    const githubCode = req.nextUrl.searchParams.get("code");
 
-  const response = await getAccessToken(githubCode);
-  return response;
+    if (!githubCode) {
+      const error = new Error("No GitHub code provided");
+      error.status = 400;
+      throw error;
+    }
+
+    const response = await getAccessToken(githubCode);
+    return Response.json(response);
+  } catch (error) {
+    return Response.json({ error: error.message }, { status: error.status });
+  }
 }
 
 /**
- * Exchanges a session code for an access token from GitHub.
+ * Exchanges code from GitHub for access token and then fetches user name and email with the access token.
  *
  * @param {string} githubCode - the code from GitHub to exchange.
  * @returns {Response} the response object with the access token or error message.
  */
 async function getAccessToken(githubCode) {
-  const result = await fetch("https://github.com/login/oauth/access_token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
-      code: githubCode,
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-    }),
-  });
+  try {
+    const result = await fetch("https://github.com/login/oauth/access_token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        code: githubCode,
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+      }),
+    });
 
-  const githubResponse = await result.json();
-  const githubAccessToken = githubResponse.access_token;
+    const githubResponse = await result.json();
+    const githubAccessToken = githubResponse.access_token;
 
-  if (!githubAccessToken) {
-    return new Response("Failed to retrieve access token", { status: 500 });
+    if (!githubAccessToken) {
+      const error = new Error("Failed to retrieve access token");
+      error.status = 500;
+      throw error;
+    }
+
+    const githubUserResult = await fetch("https://api.github.com/user", {
+      headers: {
+        Authorization: `token ${githubAccessToken}`,
+      },
+    });
+
+    const githubUserData = await githubUserResult.json();
+
+    if (!githubUserData) {
+      const error = new Error("Failed to retrieve user data");
+      error.status = 500;
+      throw error;
+    }
+
+    const githubEmail = githubUserData.email;
+    const githubName = githubUserData.name;
+
+    return {
+      accessToken: githubAccessToken,
+      userData: { email: githubEmail, name: githubName },
+    };
+  } catch (error) {
+    return Response.json({ error: error.message }, { status: error.status });
   }
-  return new Response(JSON.stringify({ access_token: githubAccessToken}), { status: 200 });
 }
