ecd674aba0dd49f5934a3cdf3d0728c445e2414c
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-15 20:43:45 +0200

    Add getCookie function and move logic to get cookie

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -2,13 +2,6 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
-  const cookieStorage = await cookies();
-  const jwtToken = cookieStorage.get("jwt-token");
-
-  if (!jwtToken) {
-    redirect("/");
-  }
-
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -18,4 +11,16 @@ export default async function Dashboard() {
   );
 }
 
-async function getBooks() {}
+async function getCookie() {
+  const cookieStorage = await cookies();
+  const jwtToken = cookieStorage.get("jwt-token");
+
+  if (!jwtToken) {
+    redirect("/");
+  }
+  return jwtToken.value;
+}
+
+async function getBooks() {
+  
+}
