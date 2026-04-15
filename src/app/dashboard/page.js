bdb3dae6e3de802b5f5927088776d152bda47e7c
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-15 14:53:43 +0200

    Get the jwt-token in dashboard page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,10 @@
-export default function Home() {
-  return (
+import { cookies } from "next/headers";
+
+export default async function Dashboard() {
+  const cookieStorage = await cookies();
+  const jwtToken = cookieStorage.get('jwt-token');
+
+    return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>Dashboard</h1>
