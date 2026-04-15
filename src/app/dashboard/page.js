7feed92bda844fffa3596f0bba72e0bc7fc1ffb1
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-15 14:57:31 +0200

    Redirect to home page if no token provided

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,10 +1,15 @@
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
   const cookieStorage = await cookies();
-  const jwtToken = cookieStorage.get('jwt-token');
+  const jwtToken = cookieStorage.get("jwt-token");
 
-    return (
+  if (!jwtToken) {
+    redirect("/");
+  }
+
+  return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>Dashboard</h1>
@@ -12,3 +17,5 @@ export default async function Dashboard() {
     </div>
   );
 }
+
+async function getBooks() {}
