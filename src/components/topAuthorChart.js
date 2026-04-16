626ab01935544d5233ad731dda109e4ae1687291
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-16 14:56:12 +0200

    Add TopAuthorChart function and map the data to chart format

--- src/components/topAuthorChart.js
+++ src/components/topAuthorChart.js
@@ -4,3 +4,14 @@ import { bar } from "react-chartjs-2";
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+export default function TopAuthorChart({ authors }) {
+    const authorData = {
+        labels: authors.map(author => author.name),
+        datasets: [{
+            label: "Number of Books",
+            data: authors.map(author => author.amountOfBooks),
+            backgroundColor: "rgba(53, 162, 235, 0.5)",
+        }]
+    };
+}
