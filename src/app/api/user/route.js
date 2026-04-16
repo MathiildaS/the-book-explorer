31f09589f06db0c565ec668a3e86622958553217
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-16 11:00:03 +0200

    Refactor. Replace req.nextUrl with req.url in route handlers for reliability

---
+++ src/app/api/user/route.js
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+/**
+ * Logs out user by removing jwt token in cookie.
+ *
+ * @param {Request} req - the request object to log out an user
+ * @returns {Response} the response object with a redirect to the home page if ok or error page if error.
+ */
+export async function POST(req) {
+  try {
+    await emptyCookie();
+  } catch (error) {
+    const message = "Could not log out user, please try again."
+    return errorHandling(req, message);
+  }
+  return NextResponse.redirect(new URL("/", req.url));
+}
+
+/**
+ * Removes token in cookie.
+ */
+async function emptyCookie() {
+  const cookieStorage = await cookies();
+  cookieStorage.delete("jwt-token");
+}
+
+/**
+ * Handles errors by redirecting to a custom page with specific error messages. 
+ *
+ * @param {object} req - the req-object from request
+ * @param {string} message - the error message. 
+ */
+function errorHandling(req, message) {
+  const url = new URL("/error", req.url);
+  url.searchParams.set("message", message);
+  return NextResponse.redirect(url)
+}
