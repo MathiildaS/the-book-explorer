429a1b7483c75efc7a5f1e0718a098361545fbf8
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-16 15:07:49 +0200

    Add chart options and return the chart

--- src/components/topAuthorChart.js
+++ src/components/topAuthorChart.js
@@ -1,17 +1,66 @@
-"use client"
+"use client";
 
-import { bar } from "react-chartjs-2";
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
 
 export default function TopAuthorChart({ authors }) {
-    const authorData = {
-        labels: authors.map(author => author.name),
-        datasets: [{
-            label: "Number of Books",
-            data: authors.map(author => author.amountOfBooks),
-            backgroundColor: "rgba(53, 162, 235, 0.5)",
-        }]
-    };
+  const authorData = {
+    labels: authors.map((author) => author.name),
+    datasets: [
+      {
+        label: "Number of Books",
+        data: authors.map((author) => author.amountOfBooks),
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
+  const chartOptions = {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "Number of Books",
+        },
+      },
+      x: {
+        title: {
+          display: true,
+          text: "Authors",
+        },
+      },
+    },
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            return `${context.dataset.label}: ${context.parsed.y}`;
+          },
+        },
+      },
+      legend: {
+        display: false,
+      },
+    },
+  };
+  return <Bar data={authorData} options={chartOptions} />;
 }
