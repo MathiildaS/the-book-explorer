9aadad430e2e872115c9e84bce133ba349367726
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-16 14:43:13 +0200

    When expired session, remove cookie and redirect to error page with error message

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -11,10 +11,25 @@ export async function POST(req) {
   try {
     await emptyCookie();
   } catch (error) {
-    const message = "Could not log out user, please try again."
+    const message = "Could not log out user, please try again.";
     return errorHandling(req, message);
   }
   return NextResponse.redirect(new URL("/", req.url));
+}
+
+export async function GET(req) {
+  try {
+    await emptyCookie();
+    return NextResponse.redirect(
+      new URL(
+        "/error?message=Your session has expired. Please log in again.",
+        req.url,
+      ),
+    );
+  } catch (error) {
+    const message = "Could not clear the user session.";
+    return errorHandling(req, message);
+  }
 }
 
 /**
@@ -26,13 +41,13 @@ async function emptyCookie() {
 }
 
 /**
- * Handles errors by redirecting to a custom page with specific error messages. 
+ * Handles errors by redirecting to a custom page with specific error messages.
  *
  * @param {Request} req - the req-object from request
- * @param {string} message - the error message. 
+ * @param {string} message - the error message.
  */
 function errorHandling(req, message) {
   const url = new URL("/error", req.url);
   url.searchParams.set("message", message);
-  return NextResponse.redirect(url)
+  return NextResponse.redirect(url);
 }
