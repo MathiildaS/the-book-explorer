f2180e26a9e8c63732f22d0af3c0f984ff0699e0
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-16 14:07:06 +0200

    Add type button to function

--- src/components/button.js
+++ src/components/button.js
@@ -1,5 +1,7 @@
-export default function Button({children, onClick}) {
+export default function Button({children, onClick, type = "button"}) {
     return (
-        <button className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700" onClick={onClick}>{children}</button>
+        <button className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700" onClick={onClick} type={type}>
+            {children}
+        </button>
     )
 }
