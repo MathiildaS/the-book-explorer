62fdf6a57b2ca263cd553c74ce87e0e28c028095
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-16 14:07:47 +0200

    Check if token, toggle button to login or logout

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,29 @@
-"use client";
-
+import { cookies } from "next/headers";
 import Button from "./button.js";
 
 /**
  * Component to display the header on all pages of the app.
  * @returns The header component.
  */
-export default function Header() {
-  function loginBtn() {
-    window.location.href = "/api/githubAuth";
-  }
+export default async function Header() {
+  const cookieStorage = await cookies();
+  const jwtToken = cookieStorage.get("jwt-token");
+
+  const isUserLoggedIn = !!jwtToken;
+
   return (
     <header className="w-full h-16 bg-gray-800 text-white flex items-center justify-center">
       <h1 className="text-xl font-bold">WT Assignment Dashboard</h1>
       <nav>
-        <Button onClick={loginBtn}>Login with GitHub</Button>
+        {isUserLoggedIn ? (
+          <form action="/api/user" method="post">
+            <Button type="submit">Logout</Button>
+          </form>
+        ) : (
+          <a href="/api/githubAuth">
+            <Button>Login with GitHub</Button>
+          </a>
+        )}
       </nav>
     </header>
   );
