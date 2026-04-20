2fd4c8acd9b81b79a74ae0bd1b605dc6edd1b090
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-20 11:17:24 +0200

    Fix issue with displaying error-message by awaiting searchParams

--- src/app/error/page.js
+++ src/app/error/page.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
 
-export default function Errors({ searchParams }) {
-  const errorMessage = searchParams.message || "An unexpected error occurred.";
+export default async function Errors({ searchParams }) {
+  const searchParamsValue = await searchParams
+  const errorMessage = searchParamsValue.message || "An unexpected error occurred.";
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
