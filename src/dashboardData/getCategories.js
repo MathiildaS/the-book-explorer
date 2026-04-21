83998bb4fc94683b32379e792d19becf46660762
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-21 15:18:31 +0200

    Add POST request to fetch all categories from API with jwt token in Authorization header

--- src/dashboardData/getCategories.js
+++ src/dashboardData/getCategories.js
@@ -1,9 +1,26 @@
 import { getCookie } from "./jwtCookie";
 
-export async function getCategories() {
+export async function getAllCategories() {
     try {
  const jwtToken = await getCookie();
 
+    const getCategories = await fetch(process.env.NEXT_PUBLIC_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      body: JSON.stringify({
+        query: `
+        query {
+        categories {
+        id
+        name
+      }
+      }
+      `,
+      }),
+    })
     } catch (error) {
 
     }
