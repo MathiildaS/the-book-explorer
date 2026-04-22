4f7ecf8f6c550ca3cb04bb46140c616ce7171f6a
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-22 10:12:28 +0200

    Display books and filtering form with pagination in dashboard page. Add error handling for fetching books, authors and categories

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -3,6 +3,11 @@ import { getTopAuthors } from "../../dashboardData/getAuthorToplist.js";
 import { redirect } from "next/navigation";
 import TopAuthorChart from "../../clientComponents/topAuthorChart.js";
 import Link from "next/link.js";
+import FilteringForm from "../../clientComponents/filteringForm.js";
+import { getFiltering } from "../../dashboardData/getFiltering.js";
+import BookObject from "../../clientComponents/bookList.js";
+import { getPagination } from "../../dashboardData/getPagination.js";
+import { getAllCategories } from "../../dashboardData/getCategories.js";
 
 /**
  *
@@ -10,18 +15,39 @@ import Link from "next/link.js";
  */
 export default async function Dashboard({ searchParams }) {
   const searchParameters = await searchParams;
-  const bookLimit = Number(searchParameters.bookLimit) || 12;
-  const pageIndex = Number(searchParameters.pageIndex) || 0;
 
-  const allBooksResult = await getBooks(bookLimit, pageIndex);
+  const { bookLimit, pageIndex, filter, formFilter } =
+    getFiltering(searchParameters);
+
+  const allBooksResult = await getBooks(bookLimit, pageIndex, filter);
   const authorToplistResult = await getTopAuthors(20);
+  const allCategoriesResult = await getAllCategories();
 
-  if (allBooksResult.authError || authorToplistResult.authError) {
+  if (
+    allBooksResult.authError ||
+    authorToplistResult.authError ||
+    allCategoriesResult.authError
+  ) {
     return redirect("/api/user");
+  }
+
+  if (allBooksResult.fetchError || !allBooksResult.data) {
+    throw new Error("Could not fetch books");
+  }
+
+  if (authorToplistResult.fetchError || !authorToplistResult.data) {
+    throw new Error("Could not fetch top authors");
+  }
+
+  if (allCategoriesResult.fetchError || !allCategoriesResult.data) {
+    throw new Error("Could not fetch categories");
   }
 
   const { books, pageInfo } = allBooksResult.data;
   const authors = authorToplistResult.data;
+  const categories = allCategoriesResult.data;
+
+  const { prevPage, nextPage } = getPagination(pageInfo, formFilter);
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -29,64 +55,19 @@ export default async function Dashboard({ searchParams }) {
         <h1>Dashboard</h1>
         <TopAuthorChart authors={authors} />
 
+        <FilteringForm filter={formFilter} categories={categories}/>
+
         <p className="mt-4">List of Books: {books.length}</p>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
           {books.map((book) => (
-            <div
-              key={book.id}
-              className="bg-white dark:bg-zinc-900 rounded-lg shadow-md p-5 hover:shadow-lg transition"
-            >
-              <h2 className="text-lg font-semibold mb-2">{book.title}</h2>
-
-              <p className="text-sm text-gray-600 dark:text-gray-400 mb-3">
-                {book.description}
-              </p>
-
-              <div className="text-sm space-y-1">
-                <p>
-                  <span className="font-medium">Price:</span> ${book.price}
-                </p>
-                <p>
-                  <span className="font-medium">Published:</span>{" "}
-                  {book.publishMonth}/{book.publishYear}
-                </p>
-                <p>
-                  <span className="font-medium">Publisher:</span>{" "}
-                  {book.publisher.name}
-                </p>
-              </div>
-
-              <div className="mt-3 text-sm">
-                <p>
-                  <span className="font-medium">Authors:</span>{" "}
-                  {book.authors.map((a) => a.name).join(", ")}
-                </p>
-
-                <p>
-                  <span className="font-medium">Categories:</span>{" "}
-                  {book.categories.map((c) => c.name).join(", ")}
-                </p>
-              </div>
-            </div>
+            <BookObject key={book.id} book={book} />
           ))}
         </div>
 
         <div>
-          {pageInfo.prevPage && (
-            <Link
-              href={`/dashboard?bookLimit=${bookLimit}&pageIndex=${pageInfo.currentBookIndex - pageInfo.booksPerPage}`}
-            >
-              Previous Page
-            </Link>
-          )}
-          {pageInfo.nextPage && (
-            <Link
-              href={`/dashboard?bookLimit=${bookLimit}&pageIndex=${pageInfo.currentBookIndex + pageInfo.booksPerPage}`}
-            >
-              Next Page
-            </Link>
-          )}
+          {pageInfo.prevPage && <Link href={prevPage}>Previous Page</Link>}
+          {pageInfo.nextPage && <Link href={nextPage}>Next Page</Link>}
         </div>
       </main>
     </div>
