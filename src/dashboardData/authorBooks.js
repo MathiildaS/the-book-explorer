90a197da5f8a24612fae0ea47ed36f380670617f
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-22 12:20:41 +0200

    Create and add getBooksOfAuthor function and use existing book fetching logic, but based on author id

---
+++ src/dashboardData/authorBooks.js
@@ -0,0 +1,94 @@
+import { getCookie } from "./jwtCookie";
+
+export async function getBooksOfAuthor(authorBookPageData) {
+  try {
+    const { authorId, numberOfBooks, bookIndex } = authorBookPageData;
+    const jwtToken = await getCookie();
+
+    const getAuthorBooks = await fetch(process.env.NEXT_PUBLIC_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      body: JSON.stringify({
+        query: /* GraphQL */ `
+          query ($booksPerPage: Int, $currentBookIndex: Int, $authorId: ID) {
+            books(
+              booksPerPage: $booksPerPage
+              currentBookIndex: $currentBookIndex
+              authorId: $authorId
+            ) {
+              pageInfo {
+                totalBooks
+                booksPerPage
+                currentBookIndex
+                nextPage
+                prevPage
+              }
+              books {
+                id
+                title
+                description
+                price
+                publishMonth
+                publishYear
+                publisher {
+                  name
+                }
+                authors {
+                  id
+                  name
+                }
+                categories {
+                  id
+                  name
+                }
+              }
+            }
+          }
+        `,
+        variables: {
+          booksPerPage: numberOfBooks,
+          currentBookIndex: bookIndex,
+          authorId: authorId,
+        },
+      }),
+    });
+
+    const authorBooksData = await getAuthorBooks.json();
+
+    if (
+      !authorBooksData ||
+      !authorBooksData.data ||
+      !authorBooksData.data.books
+    ) {
+      throw new Error("Failed to retrieve books of author");
+    }
+
+    if (
+      authorBooksData.status === 401 ||
+      authorBooksData.errors?.[0]?.message === "Unauthorized"
+    ) {
+      const authError = {
+        authError: true,
+      };
+
+      return authError;
+    }
+
+    const authorBooksObject = {
+      authError: false,
+      data: authorBooksData.data.books,
+    };
+
+    return authorBooksObject;
+  } catch (error) {
+    const getAuthorBooksError = {
+      authError: false,
+      fetchError: true,
+    };
+
+    return getAuthorBooksError;
+  }
+}
