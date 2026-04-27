b729fa5ed3f776acd8eff17dbf954152c376e76a
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-27 23:27:40 +0200

    Create and export getBooksInCategory function and use existing book fetching logic, but based on category id

---
+++ src/dashboardData/categoryBooks.js
@@ -0,0 +1,94 @@
+import { getCookie } from "./jwtCookie";
+
+export async function getBooksInCategory(categoryBookPageData) {
+  try {
+    const { categoryId, numberOfBooks, bookIndex } = categoryBookPageData;
+    const jwtToken = await getCookie();
+
+    const getCategoryBooks = await fetch(process.env.NEXT_PUBLIC_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      body: JSON.stringify({
+        query: /* GraphQL */ `
+          query ($booksPerPage: Int, $currentBookIndex: Int, $categoryId: ID) {
+            books(
+              booksPerPage: $booksPerPage
+              currentBookIndex: $currentBookIndex
+              categoryId: $categoryId
+            ) {
+              pageInfo {
+                totalBooks
+                booksPerPage
+                currentBookIndex
+                nextPage
+                prevPage
+              }
+              books {
+                id
+                title
+                description
+                price
+                publishMonth
+                publishYear
+                publisher {
+                  name
+                }
+                authors {
+                  id
+                  name
+                }
+                categories {
+                  id
+                  name
+                }
+              }
+            }
+          }
+        `,
+        variables: {
+          booksPerPage: numberOfBooks,
+          currentBookIndex: bookIndex,
+          categoryId: categoryId,
+        },
+      }),
+    });
+
+    const categoryBooksData = await getCategoryBooks.json();
+
+    if (
+      !categoryBooksData ||
+      !categoryBooksData.data ||
+      !categoryBooksData.data.books
+    ) {
+      throw new Error("Failed to retrieve books within category");
+    }
+
+    if (
+      getCategoryBooks.status === 401 ||
+      categoryBooksData.errors?.[0]?.message === "Unauthorized"
+    ) {
+      const authError = {
+        authError: true,
+      };
+
+      return authError;
+    }
+
+    const categoryBooksObject = {
+      authError: false,
+      data: categoryBooksData.data.books,
+    };
+
+    return categoryBooksObject;
+  } catch (error) {
+    const getCategoryBooksError = {
+      authError: false,
+      fetchError: true,
+    };
+
+    return getCategoryBooksError;
+  }
+}
