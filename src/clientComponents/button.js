a33ed30692485f82ecd68e867ebfebc030461290
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-04-28 15:00:37 +0200

    Add support of className to Button component

--- src/clientComponents/button.js
+++ src/clientComponents/button.js
@@ -1,7 +1,7 @@
-export default function Button({children, onClick, type = "button"}) {
-    return (
-        <button className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700" onClick={onClick} type={type}>
-            {children}
-        </button>
-    )
+export default function Button({ children, onClick, type = "button", className = "" }) {
+  return (
+    <button onClick={onClick} type={type} className={`flex items-center gap-2 ${className}`}>
+      {children}
+    </button>
+  );
 }
